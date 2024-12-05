5acacc7bd7e669e1f438a0edf17619f65263a2c1
Feliverse <114632980+Feliverse@users.noreply.github.com>
2024-12-05 15:04:04 -0400

    Init Ruby on Rails and Bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Importa Bootstrap y su dependencia Popper.js 
+import "bootstrap" 
+import "@popperjs/core" 
+// Importa jQuery 
+import $ from "jquery" 
+window.jQuery = $ 
+window.$ = $  
